5304ea5690410475a0877dcc7a152335b731f5b8
Satria Chasana <satria6miracle@gmail.com>
2021-09-14 21:38:07 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,173 +2,147 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
-    <meta name="description" content="Respati">
-    <meta name="author" content="Kiselgroup">
-
-    <!-- Favicon -->
-    <link rel="icon" href="{{ asset('assets/img/logo-2.png') }}" type="image/x-icon" />
-
-    <!-- Title -->
-    <title>Login</title>
-
-    <!-- Bootstrap css-->
+    <title>Login V18</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" href="{{ asset('assets/img/logo-2.png') }}" />
+    <!--===============================================================================================-->
     <link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
-
-    <!-- Icons css-->
-    <link href="{{ asset('assets/plugins/web-fonts/icons.css') }}" rel="stylesheet" />
+    <!--===============================================================================================-->
     <link href="{{ asset('assets/plugins/web-fonts/font-awesome/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/plugins/web-fonts/plugin.css') }}" rel="stylesheet" />
-
-    <!-- Style css-->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/skins.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/dark-style.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/colors/default.css') }}" rel="stylesheet">
-
-    <!-- Color css-->
-    <link id="theme" rel="stylesheet" type="text/css" media="all" href="{{ asset('assets/css/colors/color.css') }}">
-
+    <!--===============================================================================================-->
+    <link href="{{ asset('assets/plugins/web-fonts/icons.css') }}" rel="stylesheet" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link href="{{ asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/timepicker/jquery.timepicker.min.css') }}" rel="stylesheet">
+    <!--===============================================================================================-->
+    <link href="{{ asset('assets/plugins/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/login/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/login/util.css') }}">
+    <!--===============================================================================================-->
 </head>
 
-<body class="main-body leftmenu">
+<body style="background-color: #666666;">
 
-    <!-- Loader -->
-    <div id="global-loader">
-        <img src="{{ asset('assets/img/loader.svg') }}" class="loader-img" alt="Loader">
-    </div>
-    <!-- End Loader -->
-
-    <!-- Page -->
-    <div class="page main-signin-wrapper" id="page-login" style="">
-
-        {{-- @if (session('sukses'))
-            <div class="alert alert-success" role="alert">
-                <button aria-label="Close" class="close" data-dismiss="alert" type="button">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                {{ session('sukses') }} &nbsp; <a href="{{ route('kirim.index') }}">Klik Disini untuk ke Menu Terkirim
-                    &nbsp;</a>
-            </div>
-        @endif --}}
-
-        @if ($errors->any())
-            <div class="alert alert-danger mg-b-0" role="alert">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <!-- Row -->
-        <div class="row signpages text-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="row row-sm">
-                        {{-- <div class="col-lg-6 col-xl-5 d-none d-lg-block text-center bg-primary details">
-                            <div class="mt-5 pt-4 p-2 pos-absolute">
-                                <div class="clearfix"></div>
-
-                                <img src="{{ asset('assets/img/logo-6.png') }}"
-                                    style="width:130%; height:auto; margin-top=-20px" class="ht-100 mb-0" alt="user">
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <span class="login100-form-title p-b-43">
+                        <img src="{{ asset('assets/img/logo-1.png') }}" style="width: 50%" alt="">
+                        @if (session('error'))
+                            <div class="alert alert-danger" role="alert">
+                                <button aria-label="Close" class="close" data-dismiss="alert" type="button">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                {{ session('error') }}
                             </div>
-                        </div> --}}
-                        <div class="col-lg-12 col-xl-12 col-xs-12 col-sm-12 login_form ">
-                            <div class="container-fluid">
-                                <div class="row row-sm">
-                                    <div class="card-body mt-2 mb-2">
-                                        @if (session('error'))
-                                            <div class="alert alert-danger" role="alert">
-                                                <button aria-label="Close" class="close" data-dismiss="alert"
-                                                    type="button">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                                {{ session('error') }}
-                                            </div>
-                                        @elseif (session('success'))
-                                            <div class="alert alert-success" role="alert">
-                                                <button aria-label="Close" class="close" data-dismiss="alert"
-                                                    type="button">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                                {{ session('success') }}
-                                            </div>
-                                        @endif
-                                        {{-- <img src="{{ asset('assets/img/brand/logo.png') }}"
-                                            class=" d-lg-none header-brand-img text-left float-left mb-4" alt="logo"> --}}
-                                        <div class="clearfix"></div>
-                                        <form method="POST" action="{{ route('login') }}">
-                                            @csrf
-                                            {{-- <h3 class="text-center mb-2">Kinarya Respati</h3> --}}
-                                            {{-- <p class="mb-4 text-muted tx-13 ml-0 text-center">Signin with Marissa</p> --}}
-                                            <div class="form-group text-left">
-                                                <label for="email">{{ __('E-Mail Address') }}</label>
-                                                <input id="email" type="email"
-                                                    class="form-control @error('email') is-invalid @enderror"
-                                                    name="email" value="{{ old('email') }}" required
-                                                    autocomplete="email" autofocus>
-                                                @error('email')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group text-left">
-                                                <label for="password">{{ __('Password') }}</label>
-                                                <input id="password" type="password"
-                                                    class="form-control @error('password') is-invalid @enderror"
-                                                    name="password" required autocomplete="current-password">
-                                                @error('password')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                            </div>
-                                            <button class="btn ripple btn-block"
-                                                style="background-color:#3195F1;color: white;">Sign In</button>
-                                        </form>
-                                        <div class="text-left mt-5 ml-0">
-
-                                            <div class="mb-1"><a target="_blank"
-                                                href="{{ route('manual-book') }}">Download Manual Book</a>
-                                            </div>
-                                            @if (Route::has('password.request'))
-                                                <div class="mb-1"><a
-                                                        href="{{ route('password.request') }}">{{ __('Lupa Password?') }}</a>
-                                                </div>
-                                            @endif
-                                            {{-- <div>Don't have an account? <a
-                                                    href="/regi">Register Here</a></div> --}}
-                                        </div>
-                                    </div>
-                                </div>
+                        @elseif (session('success'))
+                            <div class="alert alert-success" role="alert">
+                                <button aria-label="Close" class="close" data-dismiss="alert" type="button">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                {{ session('success') }}
                             </div>
+                        @endif
+                    </span>
+                    <b>
+                        <h3>Sign In</h3>
+                    </b>
+                    <p class="">untuk mengakses Building Management</p>
+
+                    <div class="wrap-input100 validate-input mt-50" data-validate="Valid email is required: ex@abc.xyz">
+                        <input id="email" type="email"
+                        class="input100 @error('email') is-invalid @enderror"
+                        name="email" value="{{ old('email') }}" required
+                        autocomplete="email" autofocus>
+                        <span class="focus-input100"></span>
+                        <span class="label-input100">Email</span>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input id="password" type="password"
+                        class="input100 @error('password') is-invalid @enderror"
+                        name="password" required autocomplete="current-password">
+                        <span class="focus-input100"></span>
+                        <span class="label-input100">Password</span>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="flex-sb-m w-full p-t-3 p-b-32">
+                        <div class="contact100-form-checkbox">
+
+                        </div>
+
+                        <div>
+                            <a href="{{ route('password.request') }}" class="txt1">
+                                Lupa Password?
+                            </a>
                         </div>
                     </div>
+
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" type="submit">
+                            Sign In
+                        </button>
+                    </div>
+
+                    <div class="text-center p-t-46 p-b-20">
+                        <span class="txt2"><a target="_blank"
+                            href="{{ route('manual-book') }}">
+                            Download Manual Book </a>
+                        </span>
+                    </div>
+                </form>
+
+                <div class="login100-more" style="background-color: #cae3fc">
+                    <img src="{{ asset('assets/img/Design Login.png') }}" style="width: 50%; margin-left:200px;"
+                        alt="">
                 </div>
             </div>
         </div>
-        <!-- End Row -->
-
     </div>
-    <!-- End Page -->
 
-    <!-- Jquery js-->
+
+
+
+
+    <!--===============================================================================================-->
     <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
-
-    <!-- Bootstrap js-->
+    <!--===============================================================================================-->
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
     <script src="{{ asset('assets/plugins/bootstrap/js/popper.min.js') }}"></script>
     <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
-
-    <!-- Select2 js-->
-    <script src="{{ asset('assets/plugins/select2/js/select2.min.js') }}"></script>
-
-    <!-- Custom js -->
-    <script src="{{ asset('assets/js/custom.js') }}"></script>
-
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets/js/select2.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets/plugins/bootstrap-daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets/login/main.js') }}"></script>
 
 </body>
 
---
+++ resources/views/auth/login1.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="description" content="Respati">
+    <meta name="author" content="Kiselgroup">
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('assets/img/logo-2.png') }}" type="image/x-icon" />
+
+    <!-- Title -->
+    <title>Login</title>
+
+    <!-- Bootstrap css-->
+    <link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+    <!-- Icons css-->
+    <link href="{{ asset('assets/plugins/web-fonts/icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/web-fonts/font-awesome/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/web-fonts/plugin.css') }}" rel="stylesheet" />
+
+    <!-- Style css-->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/skins.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/dark-style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/colors/default.css') }}" rel="stylesheet">
+
+    <!-- Color css-->
+    <link id="theme" rel="stylesheet" type="text/css" media="all" href="{{ asset('assets/css/colors/color.css') }}">
+
+</head>
+
+<body class="main-body leftmenu">
+
+    <!-- Loader -->
+    <div id="global-loader">
+        <img src="{{ asset('assets/img/loader.svg') }}" class="loader-img" alt="Loader">
+    </div>
+    <!-- End Loader -->
+
+    <!-- Page -->
+    <div class="page main-signin-wrapper" id="page-login" style="">
+
+        {{-- @if (session('sukses'))
+            <div class="alert alert-success" role="alert">
+                <button aria-label="Close" class="close" data-dismiss="alert" type="button">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                {{ session('sukses') }} &nbsp; <a href="{{ route('kirim.index') }}">Klik Disini untuk ke Menu Terkirim
+                    &nbsp;</a>
+            </div>
+        @endif --}}
+
+        @if ($errors->any())
+            <div class="alert alert-danger mg-b-0" role="alert">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <!-- Row -->
+        <div class="row signpages text-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="row row-sm">
+                        {{-- <div class="col-lg-6 col-xl-5 d-none d-lg-block text-center bg-primary details">
+                            <div class="mt-5 pt-4 p-2 pos-absolute">
+                                <div class="clearfix"></div>
+
+                                <img src="{{ asset('assets/img/logo-6.png') }}"
+                                    style="width:130%; height:auto; margin-top=-20px" class="ht-100 mb-0" alt="user">
+                            </div>
+                        </div> --}}
+                        <div class="col-lg-12 col-xl-12 col-xs-12 col-sm-12 login_form ">
+                            <div class="container-fluid">
+                                <div class="row row-sm">
+                                    <div class="card-body mt-2 mb-2">
+                                        @if (session('error'))
+                                            <div class="alert alert-danger" role="alert">
+                                                <button aria-label="Close" class="close" data-dismiss="alert"
+                                                    type="button">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                                {{ session('error') }}
+                                            </div>
+                                        @elseif (session('success'))
+                                            <div class="alert alert-success" role="alert">
+                                                <button aria-label="Close" class="close" data-dismiss="alert"
+                                                    type="button">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                                {{ session('success') }}
+                                            </div>
+                                        @endif
+                                        {{-- <img src="{{ asset('assets/img/brand/logo.png') }}"
+                                            class=" d-lg-none header-brand-img text-left float-left mb-4" alt="logo"> --}}
+                                        <div class="clearfix"></div>
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+                                            {{-- <h3 class="text-center mb-2">Kinarya Respati</h3> --}}
+                                            {{-- <p class="mb-4 text-muted tx-13 ml-0 text-center">Signin with Marissa</p> --}}
+                                            <div class="form-group text-left">
+                                                <label for="email">{{ __('E-Mail Address') }}</label>
+                                                <input id="email" type="email"
+                                                    class="form-control @error('email') is-invalid @enderror"
+                                                    name="email" value="{{ old('email') }}" required
+                                                    autocomplete="email" autofocus>
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group text-left">
+                                                <label for="password">{{ __('Password') }}</label>
+                                                <input id="password" type="password"
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    name="password" required autocomplete="current-password">
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            <button class="btn ripple btn-block"
+                                                style="background-color:#3195F1;color: white;">Sign In</button>
+                                        </form>
+                                        <div class="text-left mt-5 ml-0">
+
+                                            <div class="mb-1"><a target="_blank"
+                                                href="{{ route('manual-book') }}">Download Manual Book</a>
+                                            </div>
+                                            @if (Route::has('password.request'))
+                                                <div class="mb-1"><a
+                                                        href="{{ route('password.request') }}">{{ __('Lupa Password?') }}</a>
+                                                </div>
+                                            @endif
+                                            {{-- <div>Don't have an account? <a
+                                                    href="/regi">Register Here</a></div> --}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Row -->
+
+    </div>
+    <!-- End Page -->
+
+    <!-- Jquery js-->
+    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap js-->
+    <script src="{{ asset('assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <!-- Select2 js-->
+    <script src="{{ asset('assets/plugins/select2/js/select2.min.js') }}"></script>
+
+    <!-- Custom js -->
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+
+
+</body>
+
+</html>
